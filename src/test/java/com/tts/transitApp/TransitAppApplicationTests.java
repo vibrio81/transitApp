06e1f4c2fad8e83f81a8b59c5542d2018c98ded5
vibrio81 <lstoneporter@gmail.com>
2021-03-05 08:17:46 -0600

    Initial commit

---
+++ src/test/java/com/tts/transitApp/TransitAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.transitApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransitAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
